ac5653af2c6e6d72f91b3a8ad8bb9ae311431ebf
quadrant26 <quadrant26@163.com>
2021-07-26 19:00:00 +0800

    swagger 文档

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Query, Param } from '@nestjs/common';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { Controller, Get, Post, Body, Put, Query, Param, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
 
 class CreatePostDto {
+  @ApiProperty({ description: "帖子标题" })
   title: string;
+  @ApiProperty({ description: "帖子内容" })
   content: string;
 }
 
@@ -23,16 +25,38 @@ export class PostsController {
   @Post()
   @ApiOperation({ summary: '创建帖子' })
   // create(@Body() data, @Query() query, @Param() params) {
-  create(@Body() body) {
+  create(@Body() body: CreatePostDto) {
     return {
-      success: true
+      success: true,
+      body: body
     }
   }
 
   @Get(':id')
-  detail() {
+  @ApiOperation({ summary: '博客详情' })
+  detail(@Param('id') id: string) {
     return {
-      status: 200
+      status: 200,
+      id: id,
+    }
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: '编辑帖子' })
+  update(@Param('id') id: string, @Body() body: CreatePostDto) {
+    return {
+      status: 200,
+      id: id,
+      body: body
+    }
+  }
+
+  @Delete('id')
+  @ApiOperation({ summary: '删除帖子' })
+  del(@Param('id') id: string) {
+    return {
+      status: 200,
+      id: id
     }
   }
 }
